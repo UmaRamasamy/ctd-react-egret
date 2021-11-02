476ac30fea2aaa40a6c689417650e937d36fada1
Uma Maheswari <Virgo@Umas-MacBook-Pro.local>
2021-11-02 12:16:54 -0700

    Assignmment On React-Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import TodoList from './components/TodoList'
 import AddTodoForm from './components/AddTodoForm'
+import { BrowserRouter, Switch, Route,Link } from 'react-router-dom'
 
 
 function App() {
@@ -34,36 +35,56 @@ function App() {
     }
   }, [isLoading, todoList])
   const addTodo = (newTodo) => {
-    
-      fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`,
-           {
-             method:'POST',
-             headers:{
-              Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-              'Content-type': 'application/json'
-             },
-             body: JSON.stringify(newTodo)
-             
-           }).then(response =>response.json())
-           .then(data => setTodoList([...todoList,data]))
-          }
-    //setTodoList([...todoList, newTodo])
 
-    function removeTodo(id) {
-      const newArr = todoList.filter(function (item) {
-        return item.id !== id
-      })
-      setTodoList(newArr)
+    fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(newTodo)
 
-    }
-    return (
-      <>
-        <h1>To Do List</h1>
-        <AddTodoForm onAddTodo={addTodo} />
-        {isLoading ? <p>Loading...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
-      </>
-    );
+      }).then(response => response.json())
+      .then(data => setTodoList([...todoList, data]))
   }
+  
+  
+  //setTodoList([...todoList, newTodo])
+
+  function removeTodo(id) {
+    const newArr = todoList.filter(function (item) {
+      return item.id !== id
+    })
+    setTodoList(newArr)
+
+  }
+  return (
+    <BrowserRouter>
+    
+    <nav>
+      <ul>
+        <li><Link to="/">List 1</Link></li>
+        <li><Link to="/new">List 2</Link></li>
+      </ul>
+    </nav>
+      <Switch>
+        <Route exact path="/">
+          <>
+            <h1>To Do List</h1>
+            <AddTodoForm onAddTodo={addTodo} />
+            {isLoading ? <p>Loading...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
+          </>
+        </Route>
+        <Route path="/new" >
+          <h1>New Todo List</h1>
+        </Route>
+      </Switch>
+     
+    </BrowserRouter>
+
+  );
+}
 
 
-  export default App
+export default App
--- src/components/InputWithLabel.js
+++ src/components/InputWithLabel.js
@@ -1,15 +1,15 @@
-import React, {useRef,useEffect} from 'react'
+import React, { useRef, useEffect } from 'react'
 
 function InputWithLabel(props) {
- const inputRef = useRef();
-  useEffect (()=>{
-    if(inputRef.current){inputRef.current.focus()} 
+  const inputRef = useRef();
+  useEffect(() => {
+    if (inputRef.current) { inputRef.current.focus() }
   })
   return (
-   <>
-    <label htmlFor ="todoTitle">{props.children} </label>
-    <input value={props.todoTitle} onChange={props.handleTitleChange} name= "title" type = "text" id ="todoTitle" ref={inputRef}></input>  
-   </>
+    <>
+      <label htmlFor="todoTitle">{props.children} </label>
+      <input value={props.todoTitle} onChange={props.handleTitleChange} name="title" type="text" id="todoTitle" ref={inputRef}></input>
+    </>
   )
 }
 
