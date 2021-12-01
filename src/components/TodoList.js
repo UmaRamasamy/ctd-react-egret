b4c44eeec94e976396a3efbf2b56e9135a279341
Uma Maheswari <Virgo@Umas-MacBook-Pro.local>
2021-12-01 15:17:35 -0800

    Use of CSS

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import TodoListItem from './TodoListItem'
+import TodoListItem from './TodoListItem';
+import style from '../TodoListItem.module.css';
 
 const TodoList = ({ todoList, onRemoveTodo }) => {
+
   return (
     <div>
-      <ul>
+      <ul className={style.unorderedTodoList}>
         {todoList.map(function (item) {
           return (<TodoListItem key={item.id} todo={item} onRemoveTodo={onRemoveTodo} />)
-        })
+         })
         }
       </ul>
     </div>
   )
 }
-export default TodoList
+export default TodoList;
